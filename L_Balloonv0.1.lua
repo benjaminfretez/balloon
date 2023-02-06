8c78978e54f2d8a07ad297952f054757d044522e
Benjamin <benjaminfretez@aol.com>
2023-02-06 12:54:22 -0300

    added color functionality but it doesnt'work well on netgames\ needs to fix that

--- L_Balloonv0.1.lua
+++ L_Balloonv0.1.lua
@@ -13,23 +13,25 @@ CV_RegisterVar({
 	string = "Off"
 })
 
+
 -- Add the 'ballooncolor' cvar
 CV_RegisterVar({
 	name = "ballooncolor",
 	defaultvalue = "Red",
 	PossibleValue= {
-		string1="Red",
-		string2="Green"
-	}
+		Red = 1,
+		Green = 2,
+		Blue = 3
+	},
 	string = "Red"
 })
 
 -- Add the 'balloonadd' command
 COM_AddCommand('balloonadd', function(player)
-	local object = _G[string.upper("mt_balloon")]
+	local object = _G["MT_BALLOON"]
 	
 	if player.mo and player.mo.valid then
-		P_SpawnMobj(player.mo.x, player.mo.y, player.mo.z, object)
+		local balloon = P_SpawnMobj(player.mo.x, player.mo.y, player.mo.z, object)
 		if CV_FindVar("balloondebug").string == "On" then
 			CONS_Printf(player, "Úballoon spawned")
 			CONS_Printf(player, player.mo.x/FRACUNIT, player.mo.y/FRACUNIT, player.mo.z/FRACUNIT)
@@ -47,3 +49,24 @@ addHook("KeyDown", function(keyevent_t)
 	end
 	return false
 end)
+
+-- Add Hook for do things when the balloon spawns
+--[[ use the return value of P_SpawnMobj in a
+ 	variable and put it here as the function
+ 	argument because it returns mobj_t and we 
+ 	need that as an argument of the function 
+ 	for the MobjSpawn hook... 
+	
+	When I connect in netgames the balloons are seen in the color predefined.
+	I don;t like that
+ ]]
+ addHook("MobjSpawn", function(mobj)
+	if CV_FindVar("ballooncolor").string == "Green" then
+		mobj.color = SKINCOLOR_GREEN
+	elseif CV_FindVar("ballooncolor").string == "Blue" then
+		mobj.color = SKINCOLOR_BLUE
+	else -- Default is red
+		mobj.color = SKINCOLOR_RED
+	end
+	return true
+ end, MT_BALLOON)
